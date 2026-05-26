8bf00d1bfc223b0846296f6dacd9d9eb06dad3d8
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2026-05-26 20:22:39 -0300

    Update OrderEventEmitter to use CopyOnWriteArrayList and fix emitter cleanup logic

--- backend/realtime order monitor/src/main/java/org/claumann/realtimeordermonitor/infrastructure/sse/OrderEventEmitter.java
+++ backend/realtime order monitor/src/main/java/org/claumann/realtimeordermonitor/infrastructure/sse/OrderEventEmitter.java
@@ -7,23 +7,35 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @Component
 public class OrderEventEmitter {
 
-    private final ConcurrentHashMap<String, List<SseEmitter>> emitters = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, CopyOnWriteArrayList<SseEmitter>> emitters = new ConcurrentHashMap<>();
 
     public SseEmitter register(final String orderId) {
+        // Cria novo emitter
         final SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
-        if (emitters.containsKey(orderId)) {
-            emitters.get(orderId).add(emitter);
-            return emitter;
-        }
 
-        final Runnable cleanup = () -> emitters.remove(orderId);
+        // Registra callbacks para remover o emitter recém criado quando estiver completado
+        final Runnable cleanup = () -> {
+            final List<SseEmitter> list = emitters.get(orderId);
+            if (list != null) list.remove(emitter);
+        };
+
         emitter.onCompletion(cleanup);
         emitter.onTimeout(cleanup);
-        emitter.onError((e) -> cleanup.run());
+        emitter.onError(ex -> cleanup.run());
+
+        // Verifica se o pedido já foi adicionado ao mapa
+        // Adiciona um novo emitter a lista de emitters do pedido
+        // Se nao existir pedido registrado no mapa de emitters, cria uma lista e registra o pedido no mapa
+        if (emitters.containsKey(orderId)) {
+            emitters.get(orderId).add(emitter);
+        } else {
+            emitters.put(orderId, new CopyOnWriteArrayList<>(List.of(emitter)));
+        }
 
         return emitter;
     }
